1a418157c31144da2b952e69b480e42eafbd007b
zachariapopcorn <zachariahmad@gmail.com>
2022-12-25 22:47:44 -0800

    Another type fix, yay...

--- utils/interfaces/CommandFile.ts
+++ utils/interfaces/CommandFile.ts
@@ -3,7 +3,7 @@ import BotClient from '../classes/BotClient';
 import CommandData from './CommandData';
 
 export default interface CommandFile {
-    run: (interaction: Discord.CommandInteraction, client: BotClient, args: any) => Promise<void>,
+    run: (interaction: Discord.CommandInteraction, client: BotClient, args: any) => Promise<any>,
     slashData: Discord.SlashCommandBuilder,
     commandData: CommandData
 }
