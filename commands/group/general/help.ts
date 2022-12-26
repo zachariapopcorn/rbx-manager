8bfeda5b26133d76f9797dbf320f257fb22c0d08
zachariapopcorn <zachariahmad@gmail.com>
2022-12-25 22:47:25 -0800

    Reimplement commands.group.general

---
+++ commands/group/general/help.ts
@@ -0,0 +1,59 @@
+import Discord from 'discord.js';
+
+import BotClient from '../../../utils/classes/BotClient';
+import CommandFile from '../../../utils/interfaces/CommandFile';
+
+import { commands } from '../../..';
+
+const command: CommandFile = {
+    run: async(interaction: Discord.CommandInteraction<Discord.CacheType>, client: BotClient, args: any): Promise<any> => {
+        let helpData = {};
+        for(let i = 0; i < commands.length; i++) {
+            let commandName = commands[i].name;
+            let slashData = commands[i].slashData;
+            let commandData = commands[i].commandData;
+            if(!helpData[commandData.category]) helpData[commandData.category] = {commandHelpStrings: [], helpEmbed: null};
+            let commandString = `**${commandName}** | ${slashData.description}`;
+            helpData[commandData.category].commandHelpStrings.push(commandString);
+        }
+        let categories = Object.keys(helpData);
+        for(let i = 0; i < categories.length; i++) {
+            let embedDescription = "";
+            let categoryHelpData = helpData[categories[i]];
+            for(let i = 0; i < categoryHelpData.commandHelpStrings.length; i++) {
+                embedDescription += `${categoryHelpData.commandHelpStrings[i]}\n`;
+            }
+            let embed = client.embedMaker({title: `${categories[i]} Commands`, description: embedDescription, type: "info", author: interaction.user});
+            helpData[categories[i]].helpEmbed = embed;
+        }
+        let helpPageIndex = 0;
+        let embed = helpData[categories[helpPageIndex]].helpEmbed;
+        let msg = await interaction.editReply({embeds: [embed]}) as Discord.Message;
+        await msg.react('⬅️');
+        await msg.react('➡️');
+        let filter = (reaction: Discord.MessageReaction, user: Discord.User) => (reaction.emoji.name === "⬅️" || reaction.emoji.name === "➡️") && user.id === interaction.user.id;
+        let collector = msg.createReactionCollector({filter: filter});
+        collector.on('collect', async(reaction: Discord.MessageReaction) => {
+            if(reaction.emoji.name === "⬅️") {
+                helpPageIndex -= 1;
+                if(helpPageIndex === -1) helpPageIndex = categories.length - 1;
+            } else {
+                helpPageIndex += 1;
+                if(helpPageIndex === categories.length) helpPageIndex = 0;
+            }
+            embed = helpData[categories[helpPageIndex]].helpEmbed;
+            await msg.reactions.removeAll();
+            await msg.edit({embeds: [embed]});
+            await msg.react('⬅️');
+            await msg.react('➡️');
+        });
+    },
+    slashData: new Discord.SlashCommandBuilder()
+    .setName("help")
+    .setDescription("Gets a list of commands"),
+    commandData: {
+        category: "General Group",
+    }
+}
+
+export default command;
---
+++ commands/group/general/info.ts
@@ -0,0 +1,28 @@
+import Discord from 'discord.js';
+import roblox from 'noblox.js';
+
+import BotClient from '../../../utils/classes/BotClient';
+import CommandFile from '../../../utils/interfaces/CommandFile';
+
+const command: CommandFile = {
+    run: async(interaction: Discord.CommandInteraction<Discord.CacheType>, client: BotClient, args: any): Promise<any> => {
+        let groupInfo = await roblox.getGroup(client.config.groupId);
+        let embedDescription = "";
+        embedDescription += `**Group Name**: ${groupInfo.name}\n`;
+        embedDescription += `**Group Description**: ${groupInfo.description}\n`;
+        embedDescription += `**Group Owner**: ${groupInfo.owner.username}\n`;
+        embedDescription += `**Group Membercount**: ${groupInfo.memberCount}\n`;
+        let jrStatus = !groupInfo.publicEntryAllowed;
+        embedDescription += `**Join Requests Enabled**: ${jrStatus ? "Yes" : "No"}`;
+        let embed = client.embedMaker({title: "Group Information", description: embedDescription, type: "info", author: interaction.user});
+        return await interaction.editReply({embeds: [embed]});
+    },
+    slashData: new Discord.SlashCommandBuilder()
+    .setName("info")
+    .setDescription("Gets the information of the group"),
+    commandData: {
+        category: "General Group",
+    }
+}
+
+export default command;
