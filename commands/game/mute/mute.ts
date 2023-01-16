649b3405e269b4e82676189900a0dad4138a2456
zachariapopcorn <zachariahmad@gmail.com>
2023-01-16 14:25:32 -0800

    Reimplement commands.game.mute

---
+++ commands/game/mute/mute.ts
@@ -0,0 +1,109 @@
+import Discord from 'discord.js';
+import roblox = require('noblox.js');
+
+import BotClient from '../../../utils/classes/BotClient';
+import CommandFile from '../../../utils/interfaces/CommandFile';
+import CommandLog from '../../../utils/interfaces/CommandLog';
+import MessagingService from '../../../utils/classes/MessagingService';
+import ModerationData from '../../../utils/interfaces/ModerationData';
+import RobloxDatastore from '../../../utils/classes/RobloxDatastore';
+import CommandHelpers from '../../../utils/classes/CommandHelpers';
+
+import config from '../../../config';
+
+const database = new RobloxDatastore(config);
+const messaging = new MessagingService(config);
+
+const command: CommandFile = {
+    run: async(interaction: Discord.CommandInteraction<Discord.CacheType>, client: BotClient, args: any): Promise<any> => {
+        let logs: CommandLog[] = [];
+        let usernames = args["username"].replaceAll(" ", "").split(",");
+        let reasonData = CommandHelpers.parseReasons(usernames, args["reason"]);
+        if(reasonData.didError) {
+            let embed = client.embedMaker({title: "Argument Error", description: `You inputted an unequal amount of usernames and reasons, please make sure that these amounts are equal, or, if you wish to apply one reason to multiple people, only put that reason for the reason argument`, type: "error", author: interaction.user})
+            return await interaction.editReply({embeds: [embed]});
+        }
+        let reasons = reasonData.parsedReasons;
+        for(let i = 0; i < usernames.length; i++) {
+            let username = usernames[i];
+            let reason = reasons[i];
+            let robloxID;
+            try {
+                robloxID = await roblox.getIdFromUsername(username);
+            } catch {
+                logs.push({
+                    username: username,
+                    status: "Error",
+                    message: "The username provided is an invalid Roblox username"
+                });
+                continue;
+            }
+            username = await roblox.getUsernameFromId(robloxID);
+            try {
+                let oldData: ModerationData;
+                try {
+                    oldData = await database.getModerationData(robloxID);
+                } catch(e) {
+                    if(!(e.response.data.error === "NOT_FOUND")) {
+                        logs.push({
+                            username: username,
+                            status: "Error",
+                            message: e
+                        });
+                        continue;
+                    } else {
+                        oldData = {
+                            banData: { // Gets overridden in the setModerationData call
+                                isBanned: false,
+                                reason: ""
+                            },
+                            muteData: {
+                                isMuted: false,
+                                reason: ""
+                            }
+                        }
+                    }
+                }
+                await database.setModerationData(robloxID, {banData: {isBanned: oldData.banData.isBanned, reason: oldData.banData.reason}, muteData: {isMuted: true, reason: reason}});
+            } catch(e) {
+                logs.push({
+                    username: username,
+                    status: "Error",
+                    message: e
+                });
+                continue;
+            }
+            let didMuteError = false;
+            try {
+                await messaging.sendMessage("Mute", {username: username, reason: reason});
+            } catch(e) {
+                didMuteError = true;
+                logs.push({
+                    username: username,
+                    status: "Error",
+                    message: `Although this user is now muted, I couldn't mute them in the game because of the following error: ${e}`
+                });
+            }
+            if(!didMuteError) {
+                logs.push({
+                    username: username,
+                    status: "Success"
+                });
+            }
+            await client.logAction(`<@${interaction.user.id}> has muted **${username}** in the game for the reason of **${reason}**`);
+            continue;
+        }
+        await client.initiateLogEmbedSystem(interaction, logs);
+    },
+    slashData: new Discord.SlashCommandBuilder()
+    .setName("mute")
+    .setDescription("Mutes the inputted user(s)")
+    .addStringOption(o => o.setName("username").setDescription("The username(s) of the user(s) you wish to mute").setRequired(true))
+    .addStringOption(o => o.setName("reason").setDescription("The reason(s) of the mute(s)").setRequired(false)) as Discord.SlashCommandBuilder,
+    commandData: {
+        category: "Mute",
+        permissions: config.permissions.game.mute
+    }
+}
+
+export default command;
