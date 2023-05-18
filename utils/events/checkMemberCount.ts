398b8b4549ca3de0307e29d213fc4d062bc04456
zachariapopcorn <zachariahmad@gmail.com>
2023-05-17 19:59:59 -0700

    Complete member counting (oops)

--- utils/events/checkMemberCount.ts
+++ utils/events/checkMemberCount.ts
@@ -8,8 +8,10 @@ const oldMemberCounts: {id: number, count: number}[] = [];
 export default async function checkMemberCount(groupID: number, client: BotClient) {
     if(!client.config.counting.enabled) return;
     try {
-        let groupInfo = (await roblox.getGroup(groupID));
-        let index = oldMemberCounts.findIndex(v => v.id === groupID);
+        let groupInfo = undefined;
+        let index = undefined;
+        groupInfo = (await roblox.getGroup(groupID));
+        index = oldMemberCounts.findIndex(v => v.id === groupID);
         if(index === -1) {
             oldMemberCounts.push({id: groupID, count: groupInfo.memberCount});
             throw("Skip check");
@@ -17,19 +19,29 @@ export default async function checkMemberCount(groupID: number, client: BotClien
         if(groupInfo.memberCount === oldMemberCounts[index].count) throw("Skip check");
         let isAddition = groupInfo.memberCount > oldMemberCounts[index].count;
         let isAtGoal = groupInfo.memberCount >= client.config.counting.goal;
+        let embedTitle: string;
+        if(!isAtGoal) {
+            embedTitle = (isAddition ? "Gained Members" : "Lost Members");
+        } else {
+            embedTitle = "Goal Reached";
+        }
         let embedDescription = "";
         embedDescription += `We have ${(isAddition ? "gained" : "lost")} **${Math.abs(groupInfo.memberCount - oldMemberCounts[index].count)}** members\n`;
         embedDescription += `**Old MemberCount**: ${oldMemberCounts[index].count}\n`;
         embedDescription += `**New MemberCount**: ${groupInfo.memberCount}`;
         embedDescription += `**Goal Reached?**: ${isAtGoal ? "Yes" : "No"}`;
-        let embed = client.embedMaker({title: `${(isAddition ? "Gained Members" : "Lost Members")}`, description: embedDescription, type: "info", author: client.user});
+        let embed = client.embedMaker({title: embedTitle, description: embedDescription, type: "info", author: client.user});
         let channel = await client.channels.fetch(client.config.counting.loggingChannel) as Discord.TextChannel;
         if(channel) {
             await channel.send({embeds: [embed]});
         }
+        oldMemberCounts[index].count = groupInfo.memberCount;
     } catch(e) {
         if(e !== "Skip check") {
             console.error(`There was an error while trying to check for member counts: ${e}`);
         }
     }
+    setTimeout(async() => {
+        await checkMemberCount(groupID, client);
+    }, 5000);
 }
--- utils/interfaces/BotConfig.ts
+++ utils/interfaces/BotConfig.ts
@@ -56,7 +56,7 @@ export default interface BotConfig {
     },
     counting: {
         enabled: boolean,
-        goal: number
+        goal: number,
         loggingChannel: string
     },
     logging: {
