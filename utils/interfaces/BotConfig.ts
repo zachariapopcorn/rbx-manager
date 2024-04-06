71a1bdeb2d6d11d0ab57675fe81a14baf19b6b4e
zachariapopcorn <zachariahmad@gmail.com>
2024-04-06 15:27:39 -0700

    Remove webserver + login capabilities

--- utils/interfaces/BotConfig.ts
+++ utils/interfaces/BotConfig.ts
@@ -6,8 +6,6 @@ import RewardEntry from './RewardEntry';
 
 export default interface BotConfig {
     DISCORD_TOKEN: string,
-    ROBLOX_USERNAME: string,
-    ROBLOX_PASSWORD: string,
     ROBLOX_COOKIE: string,
     ROBLOX_API_KEY: string,
     ROVER_API_KEY: string,
